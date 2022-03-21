a8941f36997a840d344678879755a06c52d90ec4
Danilo Albuquerque <danilo.albuquerque@liferay.com>
2022-03-21 15:09:34 -0300

    adding port scanner script in python

---
+++ nerg.py
@@ -0,0 +1,40 @@
+import sys
+import socket
+import pyfiglet
+
+banner = pyfiglet.figlet_format("NERG")
+print(banner)
+
+print("Script made by me. Good dusk ツ\n")
+
+if len(sys.argv) == 2:
+	target = socket.gethostbyname(sys.argv[1])
+else:
+	print("Usage: python3 nerg.py [target]")
+	print("e.g.: python3 nerg.py 127.0.0.1")
+
+print("=" * 50)
+print("Scanning: " + target)
+print("=" * 50)
+
+try:
+	for port in range(1,65535):
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		socket.setdefaulttimeout(1)
+		conn = s.connect_ex((target, port))
+		if conn == 0:
+			print("{} - open".format(port))
+		s.close()
+except KeyboardInterrupt:
+	print("\nStopping scan...")
+	sys.exit()
+except socket.gaierror:
+	print("\nSomething went wrong with the hostname...")
+	sys.exit()
+except socket.error:
+	print("\nThe server is not respoding...")
+	sys.exit()
+
+
+# scanning the services is missing yet
+# scripts also missing
